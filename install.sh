77fd442aec145927717137fd3f6dc7998fe0201c
yunklee <50851333+yunklee@users.noreply.github.com>
2019-05-21 09:45:03 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo yum -y install epel-release
+sudo yum -y install gcc gcc-c++ python-pip python-devel atlas atlas-devel gcc-gfortran openssl-devel libffi-devel
+
+# use pip (python)
+sudo pip install --upgrade virtualenv
+# or use pip3 (python3)
+sudo pip3 install --upgrade virtualenv
+
+sudo virtualenv --system-site-packages ~/venvs/tensorflow
+source ~/venvs/tensorflow/bin/activate
+
+# optional
+pip install --upgrade numpy scipy wheel cryptography
+
+# install tensorflow (only use CPU)
+sudo pip install --upgrade tensorflow
+# or install tensorflow-gpu
+sudo pip install --upgrade tensorflow-gpu
